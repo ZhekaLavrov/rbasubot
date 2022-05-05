77e203998310e475904b3f1c3ed75f23f240eedd
ZhekaLavrov <evgeny.lavrov2002@mail.ru>
2022-05-05 20:02:44 +0700

    register models in admin panel

--- constructor/admin.py
+++ constructor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Message, Button, Trigger, TelegramUser, TelegramMessage, CallBack
 
-# Register your models here.
+admin.site.register(Message)
+admin.site.register(Button)
+admin.site.register(Trigger)
+admin.site.register(TelegramUser)
+admin.site.register(TelegramMessage)
+admin.site.register(CallBack)
